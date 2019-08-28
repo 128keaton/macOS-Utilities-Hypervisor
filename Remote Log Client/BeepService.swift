9b8688edcff12f2325b1001416070b10909678b4
Keaton Burleson <keaton.burleson@me.com>
2019-08-28 12:42:53 -0500

    Initial commit

---
+++ Remote Log Client/BeepService.swift
@@ -0,0 +1,102 @@
+//
+//  BeepService.swift
+//  Remote Log Client
+//
+//  Created by Keaton Burleson on 8/28/19.
+//  Copyright © 2019 Keaton Burleson. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+protocol BeepServiceDelegate {
+    func connectedDevicesChanged(manager : BeepService, connectedDevices: [MCPeerID])
+}
+
+class BeepService : NSObject {
+    
+    // Service type must be a unique string, at most 15 characters long
+    // and can contain only ASCII lowercase letters, numbers and hyphens.
+    private let BeepServiceType = "mac-os-utils"
+    
+    private let myPeerId = MCPeerID(displayName: "daddy")
+    private let serviceBrowser : MCNearbyServiceBrowser
+    
+    var delegate : BeepServiceDelegate?
+    
+    lazy var session : MCSession = {
+        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        return session
+    }()
+    
+    override init() {
+        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: BeepServiceType)
+        
+        super.init()
+        
+        self.serviceBrowser.delegate = self
+        self.serviceBrowser.startBrowsingForPeers()
+    }
+    
+    deinit {
+        self.serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    func beep(atPeer: MCPeerID) {
+        if session.connectedPeers.count > 0 {
+            do {
+                try self.session.send("beep".data(using: .utf8)!, toPeers: [atPeer], with: .reliable)
+            }
+            catch let error {
+                NSLog("%@", "Error for sending: \(error)")
+            }
+        }
+        
+    }
+
+    
+}
+
+extension BeepService : MCNearbyServiceBrowserDelegate {
+    
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        NSLog("%@", "foundPeer: \(peerID)")
+        NSLog("%@", "invitePeer: \(peerID)")
+        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        NSLog("%@", "lostPeer: \(peerID)")
+    }
+    
+}
+
+extension BeepService : MCSessionDelegate {
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        NSLog("%@", "peer \(peerID) didChangeState: \(state.rawValue)")
+        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices: session.connectedPeers)
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        NSLog("%@", "didReceiveData: \(data)")
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        NSLog("%@", "didReceiveStream")
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        NSLog("%@", "didStartReceivingResourceWithName")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        NSLog("%@", "didFinishReceivingResourceWithName")
+    }
+    
+}
--- Remote Log Client/ViewController.swift
+++ Remote Log Client/ViewController.swift
@@ -7,21 +7,61 @@
 //
 
 import Cocoa
+import MultipeerConnectivity
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+
+    @IBOutlet var tableView: NSTableView!
+
+    var beepService = BeepService()
+    var peers: [MCPeerID] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.beepService.delegate = self
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.peers.count
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let peer = self.peers[row]
+
+        let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView
+
+
+        if tableColumn!.identifier.rawValue == "modelIdentifier" {
+            cell?.textField?.stringValue = String(peer.displayName.split(separator: ":")[0])
+        } else {
+            cell?.textField?.stringValue = String(peer.displayName.split(separator: ":")[1])
+        }
+
+        return cell
+    }
+
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        let selectedRow = self.tableView.selectedRow
+
+        if (self.peers.indices.contains(selectedRow)) {
+            let peer = self.peers[selectedRow]
+            self.beepService.beep(atPeer: peer)
+            NSWorkspace.shared.open(URL(string: "http://\(peer.displayName.split(separator: ":")[1]):8080")!)
+        }
+    }
+}
+
+extension ViewController: BeepServiceDelegate {
+    func connectedDevicesChanged(manager: BeepService, connectedDevices: [MCPeerID]) {
+        connectedDevices.forEach { (peer) in
+            self.peers.removeAll { $0.displayName == peer.displayName }
+            self.peers.append(peer)
+        }
+
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
         }
     }
 
-
 }
-
