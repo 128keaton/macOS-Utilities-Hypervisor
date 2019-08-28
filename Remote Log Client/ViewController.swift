157334f124b7416abff45472967685220b2622c2
Keaton Burleson <keaton.burleson@me.com>
2019-08-28 00:28:32 -0500

    Initial Commit

---
+++ Remote Log Client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Remote Log Client
+//
+//  Created by Keaton Burleson on 8/28/19.
+//  Copyright © 2019 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
